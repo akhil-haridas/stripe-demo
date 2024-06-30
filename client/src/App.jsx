74ab9445b0d1644a1970570b7ee79a317592d086
akhil-dignizant <150029159+akhil-dignizant@users.noreply.github.com>
2024-06-30 15:14:27 +0530

    redirect to the payment gateway

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 
     const response = await axios.post('http://localhost:3000/payment')
     if (response && response.status === 200) {
+      window.location.href = response.data.url
       console.log(response.data);
     }
   }
